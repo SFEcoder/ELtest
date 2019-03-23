1a1f3e20c4cccdb19bd2262a0b0e08f72cefb24c
SFEcoder <44322223+SFEcoder@users.noreply.github.com>
2019-03-24 01:43:23 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+//:HelloWorld.java
+
+  /** 我的第一个注释文档的Java程序。
+   *@author Doctor Zhao
+   *@version 1.1
+*/
+
+public class HelloWorld {
+  /** Sole entry point to class & application
+   * param args array of string arguments
+   *@return No return value
+   *@exception No exception thrown 
+*/
+	public static void main(String[] args) {
+		
+     System.out.print("Hello World!");
+	}
+}
+///:~
